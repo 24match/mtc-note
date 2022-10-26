f190369b1e1e3538b306ad6df41c9d11a18ac495
Elliot <lpy529075717@icloud.com>
2022-10-26 15:07:47 +0800

    backup

--- DataStructureAndAlgorithms/Algorithms/src/SelectionSort/SelectionSort.java
+++ DataStructureAndAlgorithms/Algorithms/src/SelectionSort/SelectionSort.java
@@ -5,28 +5,30 @@ package SelectionSort;
  */
 public class SelectionSort {
 
-    public static void sort(int[] arr){
+    public static void sort(int[] arr) {
         int n = arr.length;
-        for( int i = 0 ; i < n ; i ++ ){
+        for (int i = 0; i < n; i++) {
             // 寻找[i, n)区间里的最小值的索引
             int minIndex = i;
-            for( int j = i + 1 ; j < n ; j ++ )
-                if( arr[j] < arr[minIndex] )
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[minIndex])
                     minIndex = j;
             //数据交换不同索引位置数据
-            swap( arr , i , minIndex);
+            swap(arr, i, minIndex);
         }
     }
+
     private static void swap(int[] arr, int i, int j) {
         int t = arr[i];
         arr[i] = arr[j];
         arr[j] = t;
     }
+
     public static void main(String[] args) {
 
-        int[] arr = {10,9,8,7,6,5,4,3,2,1};
+        int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         SelectionSort.sort(arr);
-        for( int i = 0 ; i < arr.length ; i ++ ){
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
             System.out.print(' ');
         }
